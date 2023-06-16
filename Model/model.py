586cb6ccf837fe62ef35e8d37275c5b804d39195
Jonathan Hirsch <jonathan.hirsch2011@gmail.com>
2023-06-16 13:58:24 -0700

    Built initial model.

---
+++ Model/model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class RecommendationModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Create nn.Layers capable of handling the shapes of the data
+        self.layer1 = nn.Linear(in_features=12, out_features=64)
+        self.relu = nn.ReLU()
+        self.layer2 = nn.Linear(in_features=64, out_features=5)
+
+    def forward(self, x):
+        return self.layer2(self.relu(self.layer1(x)))  # x -> layer1 -> layer2 -> output
+
